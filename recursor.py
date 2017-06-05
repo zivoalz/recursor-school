ee26363b37525a73314a326162ef8a36d72c6034
Adam Ziv <zivoalz@hotmail.com>
2017-06-05 08:04:54 -0400

    Update recursor.py
    added graphs -- using pylab

--- recursor.py
+++ recursor.py
@@ -1,18 +1,38 @@
 
 # path of daily returns
 #prints a path of daily stock returns
+# Nassim Taleb's - dynanic hedging exercise
 # S subscript t = S subcript t-1  * Exp(-1/2 sigma^2 + sigma * sqrt(t) * W subscript t)
 
 import random
 import math
+import numpy
+import pylab
 
 random.seed()
 annualVol = .157 # annual standard deviation of returns
 annualTradingDays = 248
 assetPrice = 100.00 # initial asset price
 
+prices =[]
+days = []
+prices.append(assetPrice)
+days.append(0) # starting time
+
 # path of daily returns for year with 248 days of trading
-for i in range(annualTradingDays):
+for i in range(1 , annualTradingDays + 1): # range goes to -1 of max
     ranNorm = random.normalvariate(0,1)
+    days.append(i)
     assetPrice = assetPrice * math.exp(-0.5 * math.pow(annualVol,2.0) * (1.0/annualTradingDays) + annualVol * math.pow(1.0/annualTradingDays, .5) * ranNorm)
-    print assetPrice
+    prices.append(assetPrice)
+else:
+    print prices
+    print days
+#pl.plot(days, prices)
+
+# Make an array of x values
+
+
+# use pylab to plot x and y
+pylab.plot(days, prices)
+pylab.show()
